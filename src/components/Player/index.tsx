c5214b3344a4fa6661c86796e852e4e5152a19fb
Camila Mizumoto <camila.yuri.m@gmail.com>
2021-04-20 21:56:22 -0300

    add player component and styles

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -2,8 +2,15 @@ import styles from './styles.module.scss';
 
 export function Player () {
   return (
-    <div>
-
+    <div className={styles.playerContainer}>
+      <header>
+        <img src="/playing.svg" alt="Tocando agora"/>
+        <strong>Tocando agora</strong>
+      </header>
+      <div className={styles.emptyPlayer}>
+        <strong>Selecione um podcast para ouvir</strong>
+      </div>
+      <footer></footer>
     </div>
   );
 }
