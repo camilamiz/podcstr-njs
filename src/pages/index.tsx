6e1946181efe88b11de4468b1d88056964f5326b
Camila Mizumoto <camila.yuri.m@gmail.com>
2021-04-21 20:37:43 -0300

    format api response

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -43,6 +43,9 @@
 
 import { GetStaticProps } from 'next';
 import { api } from '../services/api';
+import ptBR from 'date-fns/locale/pt-BR'
+import { format, parseISO } from 'date-fns'
+import { convertDurationToTimeString } from '../utils/convertDurationToTimeString';
 
 type HomeProps = {
   episodes: Episode[];
@@ -51,7 +54,8 @@ type HomeProps = {
 type Episode = {
     id: string;
     title: string;
-    memebers: string;
+    members: string;
+    published_at: string;
     //..
 }
 
@@ -72,9 +76,23 @@ export const getStaticProps: GetStaticProps = async () => {
     }
   })
 
+  const episodes = data.map(episode => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      publishedAt: format(parseISO(episode.published_at), 'd MM yy', {locale: ptBR}),
+      duration: Number(episode.file.duration),
+      durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
+      description: episode.description,
+      url: episode.file.url,
+    };
+  })
+
   return {
     props: {
-      episodes: data,
+      episodes,
     },
     revalidate: 60 * 60 * 8,
   }
