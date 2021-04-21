85f72d4e6f6831e73674ffbe304ce61ae1c8b03c
Camila Mizumoto <camila.yuri.m@gmail.com>
2021-04-20 21:09:56 -0300

    add header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,12 @@
+import styles from './styles.module.scss';
+
 export function Header () {
+  const currentDate = new Date().toLocaleDateString()
   return (
-    <header>
+    <header className={styles.headerContainer}>
+      <img src="/logo.svg" alt="Podcastr logo"/>
+      <p>O melhor para você ouvir, sempre</p>
+      <span>Qui, 8 Abril</span>
     </header>
   );
 }
